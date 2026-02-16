a2348df8e1510119a4d6a1981358190ff81a9a9e
akamugin <stella.lee@vanderbilt.edu>
2026-02-15 14:46:41 -1000

    Initial Commit

---
+++ Steady/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Steady
+//
+//  Created by Stella Lee on 2/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Steady/SteadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteadyApp.swift
+//  Steady
+//
+//  Created by Stella Lee on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct SteadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
